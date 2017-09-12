2ec4242f3ab914737306524f128242de81ae32bc
Vaishnavi <jyvaishu@gmail.com>
2017-09-12 12:59:22 +0530

    Initial Commit

---
+++ app/src/main/java/com/anault/antiassualt/AA1.java
@@ -0,0 +1,13 @@
+package com.anault.antiassualt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AA1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aa1);
+    }
+}
